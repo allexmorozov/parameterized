2823fbd9b9d55792b08febb03e0316b65d50d3a7
allexmorozov <ouksss33@gmail.com>
2022-02-26 18:26:14 +0300

    add steps

--- src/test/java/yandex/ouksss/RegistrationParametrizedTest.java
+++ src/test/java/yandex/ouksss/RegistrationParametrizedTest.java
@@ -17,7 +17,7 @@ public class RegistrationParametrizedTest {
     @BeforeEach
     void precondition() {
         Configuration.browserSize = ("1920x1080");
-        open("https://demoqa.com/text-box");
+        open("https://demoqa.com/text-box");            // Открытие страницы с формой
     }
 
     @AfterEach
@@ -28,9 +28,9 @@ public class RegistrationParametrizedTest {
     @ValueSource(strings = {"Alex", "Bob"})
     @ParameterizedTest(name = "Проверка отображения формы регистрации \"{0}\"")
     void userNameTest(String userName) {
-        $("#userName").setValue(userName);
-        $("#submit").click();
-        $("#output").shouldHave(text(userName));
+        $("#userName").setValue(userName);                     // Ввести имя пользователя в форму
+        $("#submit").click();                                  // Нажать кнопку Submit
+        $("#output").shouldHave(text(userName)); // Проверить правильность введенных данных
     }
 
     @CsvSource({
@@ -39,10 +39,10 @@ public class RegistrationParametrizedTest {
     })
     @ParameterizedTest(name = "Проверка отображения формы регистрации \"{0}\"")
     void complexTest(String userName, String userEmail, String userAddress) {
-        $("#userName").setValue(userName);
-        $("#userEmail").setValue(userEmail);
-        $("#currentAddress").setValue(userAddress);
-        $("#submit").click();
-        $("#output").shouldHave(text(userName), text(userEmail), text(userAddress));
+        $("#userName").setValue(userName);                     // Ввести имя пользователя в форму
+        $("#userEmail").setValue(userEmail);                   // Ввести почту пользователя в форму
+        $("#currentAddress").setValue(userAddress);            // Ввести адрес пользователя в форму
+        $("#submit").click();                                  // Нажать кнопку Submit
+        $("#output").shouldHave(text(userName), text(userEmail), text(userAddress)); // Проверить правильность введенных данных
     }
 }
