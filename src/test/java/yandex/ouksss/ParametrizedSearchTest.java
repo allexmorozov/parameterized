a1df011766a2d84b90709a095dee2535520bcd32
allexmorozov <ouksss33@gmail.com>
2022-02-26 11:48:32 +0300

    delete garbage

--- src/test/java/yandex/ouksss/ParametrizedSearchTest.java
+++ src/test/java/yandex/ouksss/ParametrizedSearchTest.java
@@ -3,8 +3,6 @@ package yandex.ouksss;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -50,16 +48,17 @@ public class ParametrizedSearchTest {
         Selenide.$$("li.serp-item").find(text(expectedText)).shouldBe(visible);
 
     }
-    static Stream<Arguments> mixedArgumentsTestDataProvider(){
-return Stream.of(
-        Arguments.of("SElenide", List.of(1,2,4), true),
-        Arguments.of("JUnit 5", List.of(5,6,7), false)
-);
+
+    static Stream<Arguments> mixedArgumentsTestDataProvider() {
+        return Stream.of(
+                Arguments.of("SElenide", List.of(1, 2, 4), true),
+                Arguments.of("JUnit 5", List.of(5, 6, 7), false)
+        );
     }
 
     @MethodSource(value = "mixedArgumentsTestDataProvider")
-@ParameterizedTest
-    void mixedArgumentsTest(String firstArg, List<Integer> secondArg, boolean aBooleanValue  ){
-        System.out.println("String" + firstArg +  "List" + secondArg.toString() + "boolean" + aBooleanValue);
+    @ParameterizedTest
+    void mixedArgumentsTest(String firstArg, List<Integer> secondArg, boolean aBooleanValue) {
+        System.out.println("String" + firstArg + "List" + secondArg.toString() + "boolean" + aBooleanValue);
     }
 }
