1a9f7ceb7aca29c392de8b2b1c225a928d6f614e
allexmorozov <ouksss33@gmail.com>
2022-02-26 13:42:05 +0300

    add @CsvSource

--- src/test/java/yandex/ouksss/RegistrationParametrizedTest.java
+++ src/test/java/yandex/ouksss/RegistrationParametrizedTest.java
@@ -3,29 +3,48 @@ package yandex.ouksss;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationParametrizedTest {
 
     @BeforeEach
-    void precondition(){
+    void precondition() {
         Configuration.browserSize = ("1920x1080");
-        open("https://demoqa.com/automation-practice-form");
+        open("https://demoqa.com/text-box");
     }
+
     @AfterEach
-void afterEach(){
+    void afterEach() {
         Selenide.closeWebDriver();
     }
+
     @ValueSource(strings = {"Alex", "Bob"})
     @ParameterizedTest(name = "Проверка отображения формы регистрации \"{0}\"")
-    void openTest(String testData){
-        $("#firstName").setValue(testData);
+    void userNameTest(String userName) {
+        $("#userName").setValue(userName);
+        $("#submit").click();
+        $("#name").shouldHave(text(userName));
     }
+    @CsvSource({
+            "Alex, alex@email.com",
+            "Bob, bob@email.com"
+    })
+    @ParameterizedTest(name = "Проверка отображения формы регистрации \"{0}\"")
+    void complexTest(String userName, String userEmail) {
+        $("#userName").setValue(userName);
+        $("#userEmail").setValue(userEmail);
+        $("#submit").click();
+        $("#name").shouldHave(text(userName));
+        $("#email").shouldHave(text(userEmail));
+    }
+
+
+
 }
